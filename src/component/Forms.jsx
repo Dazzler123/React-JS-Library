45aa2eb1a701f7076b6abc19121f2820b7be54b3
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-01 16:11:06 +0530

    Content updated

--- src/component/Forms.jsx
+++ src/component/Forms.jsx
@@ -25,22 +25,6 @@ export const Forms = () => {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Forms</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse justify-content-end me-4" id="navbarNav">
-                        <button type="button" className="btn btn-primary me-3">Form</button>
-                        <button type="button" className="btn btn-secondary">Other</button>
-                    </div>
-                </div>
-            </nav>
-            <br/>
-            <br/>
             <form className="col-8 mx-auto" onSubmit={handleonclicksubmit}>
                 <label htmlFor="exampleInputName1" className="form-label font-monospace">Name</label>
                 <input value={name} type="text" className="form-control" id="exampleInputName1"
