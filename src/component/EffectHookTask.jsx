963020096b19fe2be6bd4afade0ade29df169c31
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-01 11:33:44 +0530

    Content updated

--- src/component/EffectHookTask.jsx
+++ src/component/EffectHookTask.jsx
@@ -1,13 +1,34 @@
+import {useState} from "react";
+
 export const EffectHookTask = () => {
-  return (
-      <div>
-          <div className="d-flex btn-group col-6 justify-content-center mt-5 gap-5 mx-auto row" role="group"
-               aria-label="Default button group">
-              <button type="button" className="btn btn-primary col-2">Post</button>
-              <button type="button" className="btn btn-secondary col-2">Comments</button>
-              <button type="button" className="btn btn-info col-2">Photos</button>
-              <button type="button" className="btn btn-success col-2">News</button>
-          </div>
-      </div>
-  )
+
+    const [value,setValue] = useState();
+
+    const handleonclickpost = () => {
+
+    }
+
+    const handleonclickcomment = () => {
+
+    }
+
+    const handleonclickphotos = () => {
+
+    }
+
+    const handleonclicknews = () => {
+
+    }
+
+    return (
+        <div>
+            <div className="d-flex btn-group col-6 justify-content-center mt-5 gap-5 mx-auto row" role="group"
+                 aria-label="Default button group">
+                <button onClick={handleonclickpost} type="button" className="btn btn-primary col-2">Post</button>
+                <button onClick={handleonclickcomment} type="button" className="btn btn-secondary col-2">Comments</button>
+                <button onClick={handleonclickphotos} type="button" className="btn btn-info col-2">Photos</button>
+                <button onClick={handleonclicknews} type="button" className="btn btn-success col-2">News</button>
+            </div>
+        </div>
+    )
 }
