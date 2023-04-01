65e257846c4f5e876ed4c4160cd540d234f0845e
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-01 12:46:39 +0530

    Fetched dummy sample data from JSON Placeholder

--- src/component/EffectHookTask.jsx
+++ src/component/EffectHookTask.jsx
@@ -1,32 +1,41 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 export const EffectHookTask = () => {
 
-    const [value,setValue] = useState();
+    const [value, setValue] = useState("");
+    const [data, setData] = useState();
+
+    useEffect(()=> {
+        // console.log("Hello")
+        fetch(`https://jsonplaceholder.typicode.com/${value}`)
+            .then(response => response.json())
+            .then(json => setData(JSON.stringify(json)))
+    });
 
     const handleonclickpost = () => {
-        setValue("Your Posts");
+        setValue("posts/1");
     }
 
     const handleonclickcomment = () => {
-        setValue("Your Comments");
+        setValue("comments");
     }
 
     const handleonclickphotos = () => {
-        setValue("Your Photos");
+        setValue("photos/1");
     }
 
     const handleonclicknews = () => {
-        setValue("Your News");
+        setValue("todos/1");
     }
 
     return (
         <div>
-            <h1 className="mt-5 text-center fs-1 fw-bolder">{value}</h1>
-            <div className="d-flex btn-group col-6 justify-content-center mt-5 gap-5 mx-auto row" role="group"
+            <pre className="mt-5 text-center fs-1 fw-bolder">{data}</pre>
+            <div className="d-flex btn-group col-6 justify-content-center mt-5 gap-3 mx-auto row" role="group"
                  aria-label="Default button group">
                 <button onClick={handleonclickpost} type="button" className="btn btn-primary col-2">Post</button>
-                <button onClick={handleonclickcomment} type="button" className="btn btn-secondary col-2">Comments</button>
+                <button onClick={handleonclickcomment} type="button" className="btn btn-secondary col-2">Comments
+                </button>
                 <button onClick={handleonclickphotos} type="button" className="btn btn-info col-2">Photos</button>
                 <button onClick={handleonclicknews} type="button" className="btn btn-success col-2">News</button>
             </div>
