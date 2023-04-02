52e55ee04ef2e8f60e991f7e8461e52cfb54de5f
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-02 19:07:30 +0530

    Content updated

--- src/component/AddMinusView.jsx
+++ src/component/AddMinusView.jsx
@@ -2,23 +2,23 @@ import {useState} from "react";
 
 export const AddMinusView = () => {
 
-    //newvalue is the variable (first argument)
-    //setNewValue is the function which controls the newvalue variable (second argument)
+    //value is the variable (first argument)
+    //setNewValue is the function which controls the value variable (second argument)
     //this is an array
     //must give the initial state ---> useState(0)
-    const [newvalue,setNewValue]= useState(0);
+    const [value,setValue]= useState(0);
 
     // let count = 0;
 
     const handleonclickadd = () => {
-        setNewValue(newvalue + 1);
+        setValue(value + 1);
         // count++;
     }
 
     const handleonclickminus = () => {
-        setNewValue(newvalue - 1);
+        setValue(value - 1);
 
-        if (newvalue === 0) {
+        if (value === 0) {
             alert("Value is zero.");
         }
         // count--;
@@ -26,7 +26,7 @@ export const AddMinusView = () => {
 
     return (
         <div className="overflow-hidden mt-5">
-            <h1 className="fw-bolder text-center fs-1 font-monospace mt-5">{newvalue}</h1>
+            <h1 className="fw-bolder text-center fs-1 font-monospace mt-5">{value}</h1>
             <br/>
             <br/>
             <br/>
