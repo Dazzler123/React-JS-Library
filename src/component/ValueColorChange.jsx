640ef7c834e457ea7043920c8805165b59ad0bb4
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-02 20:53:07 +0530

    Content updated

--- src/component/ValueColorChange.jsx
+++ src/component/ValueColorChange.jsx
@@ -1,19 +1,22 @@
 import {useState} from "react";
 
 export const ValueColorChange = () => {
-    const [value, setValue] = useState();
-
-    const iterateLoop = () => {
-
-    }
+    const [value, setValue] = useState(0);
 
     return (
         <>
             <div className="mb-5">
-                <h1 className="fs-1 fw-bolder font-monospace text-center mt-5">100</h1>
+                <h1 className="fs-1 fw-bolder font-monospace text-center mt-5">{value}</h1>
             </div>
             <div className="row col-12 m-0">
-                <button type="button" className="btn btn-lg btn-primary col-2 mx-auto m-0">Start</button>
+                <button type="button" className="btn btn-lg btn-primary col-2 mx-auto m-0"
+                        onClick={() => {
+                            setInterval(() => {
+                                setValue(value + 1);
+                                console.log(value)
+                            }, 1000)
+                        }}>Start
+                </button>
             </div>
         </>
     )
