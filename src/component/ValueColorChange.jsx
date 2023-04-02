4bd1ac1d8cd163eed273d4195a7c9607cc82de35
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-02 20:10:25 +0530

    Content updated

---
+++ src/component/ValueColorChange.jsx
@@ -0,0 +1,11 @@
+import {useState} from "react";
+
+export const ValueColorChange = () => {
+    const [value, setValue] = useState();
+
+    return (
+        <div>
+            <h1 className="fs-1 fw-bolder font-monospace text-center mt-5"></h1>
+        </div>
+    )
+}
