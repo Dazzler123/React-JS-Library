cb0e1e0bfb0f2da4aa52842d233800354d252ba3
Dazzler123 <dasinduhewagamage@gmail.com>
2023-04-07 21:21:19 +0530

    Content updated with logo

--- src/component/RoutingWithReactBootstrap.jsx
+++ src/component/RoutingWithReactBootstrap.jsx
@@ -1,32 +1,25 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export const RoutingWithReactBootstrap = () => {
-  return (
-      <Navbar bg="light" expand="lg">
-          <Container>
-              <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="me-auto">
-                      <Nav.Link href="#home">Home</Nav.Link>
-                      <Nav.Link href="#link">Link</Nav.Link>
-                      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                          <NavDropdown.Item href="#action/3.2">
-                              Another action
-                          </NavDropdown.Item>
-                          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                          <NavDropdown.Divider />
-                          <NavDropdown.Item href="#action/3.4">
-                              Separated link
-                          </NavDropdown.Item>
-                      </NavDropdown>
-                  </Nav>
-              </Navbar.Collapse>
-          </Container>
-      </Navbar>
-  );
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home"><img
+                    alt=""
+                    src="./tailwindcss/logo.svg"
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                />{' '}
+                    React Bootstrap</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 }
